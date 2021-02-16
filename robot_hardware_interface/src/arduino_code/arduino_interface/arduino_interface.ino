16246ed98edf2615c876f7eb957c2b1578adc104
Christian Maier <christian.maier5@gmx.net>
2021-02-16 20:05:55 +0100

    finalized arduino code for 6 joints

--- robot_hardware_interface/src/arduino_code/arduino_interface/arduino_interface.ino
+++ robot_hardware_interface/src/arduino_code/arduino_interface/arduino_interface.ino
@@ -17,18 +17,27 @@ ros::NodeHandle nh;
 
 const int num_joints = 6;
 
-float f_position = 0.0;
 float f_des_position[num_joints];
 const int steps_per_rev = 200; // 1,8° per step
 
+int i_des_pos_steps[num_joints];
 
 AccelStepper stepper1(1, 2, 3);
-int i_des_pos_steps = 0;
+AccelStepper stepper2(1, 4, 5);
+AccelStepper stepper3(1, 6, 7);
+AccelStepper stepper4(1, 8, 9);
+AccelStepper stepper5(1, 10, 11);
+AccelStepper stepper6(1, 12, 13);
+
+AccelStepper joints[num_joints] = {stepper1, stepper2, stepper3, stepper4, stepper5, stepper6};
+
+
 
 // functions-------------- 
 
 void messageCb( const std_msgs::Float32MultiArray& pos_msg){
   digitalWrite(LED_BUILTIN, HIGH-digitalRead(LED_BUILTIN));
+  
   for(int i = 0; i < num_joints; i++){
     f_des_position[i] = pos_msg.data[i];
 
@@ -36,8 +45,11 @@ void messageCb( const std_msgs::Float32MultiArray& pos_msg){
 }
 
 void Callback(const robot_hardware_interface::pos_service::Request & req, robot_hardware_interface::pos_service::Response & res)
-{
-   res.joint_pos[0] = f_position;
+{ 
+  // pos service, delivers information of joint position to controller
+  for(int i = 0; i < num_joints; i++){
+    res.joint_pos[i] = calc_pos_in_rad(joints[i].currentPosition());
+  }
 }
 
 int calc_pos_in_steps(float pos_in_rad){
@@ -59,36 +71,46 @@ ros::Subscriber<std_msgs::Float32MultiArray> sub("ard_des_joint_position_1", &me
 ros::ServiceServer<robot_hardware_interface::pos_service::Request, robot_hardware_interface::pos_service::Response> service("ard_pos_service", &Callback );
 
 void setup()
-  {
+{
     pinMode(LED_BUILTIN, OUTPUT);
     nh.initNode();
     nh.advertiseService(service);
     delay(1);
     nh.subscribe(sub);
 
+    // init variables and steppers
     for(int i = 0; i < num_joints; i++){
       f_des_position[i] = 0.0;
       }
-
-    stepper1.setMaxSpeed(400);
-    stepper1.setAcceleration(200);
-
+    for(int i = 0; i < num_joints; i++){
+      i_des_pos_steps[i] = 0.0;
+      }
+      
+    for (int i = 0; i < num_joints; i++){
+      joints[i].setMaxSpeed(400);
+      joints[i].setAcceleration(200);
     }
+
+}
 
 // main loop-------------- 
 
 void loop()
 {
-  // check if the desired position has changed, if yes then set the new position
-  int des_pos = calc_pos_in_steps(f_des_position[0]);
-  if(des_pos != i_des_pos_steps){
-    stepper1.moveTo(des_pos); 
-    i_des_pos_steps = des_pos;
-    }
+  // check if the desired position has changed, if yes then set the new position, do this for every joint
+  for(int i = 0; i < num_joints; i++){
+    
+    int des_pos = calc_pos_in_steps(f_des_position[i]);
+    if(des_pos != i_des_pos_steps[i]){
+      joints[i].moveTo(des_pos); 
+      i_des_pos_steps[i] = des_pos;
+      }
+  }
 
   // run the stepper, also update the position var to the new value
-  stepper1.run();
-  f_position = calc_pos_in_rad(stepper1.currentPosition());
+  for(int i = 0; i < num_joints; i++){
+    joints[i].run();
+  }
   
   // check ros messages, not done in every loop
   if (rosMessageCheck > ROS_MESSAGE_CHECK) {
